f2375c924c4ef217b7a28e673db1bd3c40310200
Rakshi <rakshithareddy831@gmail.com>
2026-02-05 10:04:52 +0530

    Backend Api Created

--- controller/userController.js
+++ controller/userController.js
@@ -27,24 +27,33 @@ exports.getUser=async(req,res)=>{
 
     } 
 }
-exports.getUserById = (req,res)=>{
-   const user = users.find((u)=>u.id===parseInt(req.params.id));
-   if(user===undefined)
-     return res.status(404).json({message:"User not found"});
-   res.status(200).json(user)
+exports.getUserById = async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user)
+            return res.status(401).json({message:"user not found"})
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
-exports.editUser=(req,res)=>{
-    const user = users.find((u)=>u.id===parseInt(req.params.id));
-    if(user===undefined)
-        return req.status(404).json({message:"user not found"});
-    user.name = req.body.name===undefined? user.name:req.body.name;
-    user.email = req.body.email || user.email;
-    res.status(200).json(user)
+exports.editUser=async(req,res)=>{
+    try{
+       const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true});
+       if(!user) 
+        return res.status(404).json({message:"user not found"})
+        res.status(200).json(user)
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
-exports.deleteUser = (req,res)=>{
-    const index = users.findIndex((u)=>u.id===parseInt(req.params.id));
-    if(index===-1)
-         return res.status(404).json({message:"User not found"});
-    users = users.filter((u,i)=>i==index);
-    res.status(200).json({message:"User Deleted successfully"});
+exports.deleteUser = async (req,res)=>{
+    try{
+        const user=await User.findByIdAndDelete(req.params.id);
+        if(!user)
+            return res.status(404).json({message:"user not found"})
+        res.status(200).json({message:"User deleted successfully"})
+    }catch(err){
+        res.status(500).send(err);
+    }
 }
