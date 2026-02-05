3575d646324b29387f49b20559ce056726a2ce2b
Rakshi <rakshithareddy831@gmail.com>
2026-02-05 10:50:42 +0530

    Backend Api

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 app.use(express.json());
 app.use('/api/todo', todoRoute);
 app.use('/api/user', userRoute);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`Server is running at port http://localhost:${PORT}`);
 
